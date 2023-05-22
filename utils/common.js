53857b81236169a25d516511053baeb9c2cefe14
wnicolass <nicolaswgl99@gmail.com>
2023-05-22 20:31:52 +0100

    feat: create an acceptable line pattern inside env file

--- utils/common.js
+++ utils/common.js
@@ -8,10 +8,12 @@ function regExpCompiler(pattern, flags = []) {
 const isNewLineCharRegExp = regExpCompiler(/(?:\r\n|\n|\r)/, ['g', 'm']);
 const equalSignRegExp = regExpCompiler(/=(?=.*[^=])?/);
 const isIgnorablePath = regExpCompiler(/(node_modules|.git|.venv)/, ['g']);
+const accetableLinePattern = regExpCompiler(/^\w+=(?!(.*"{2}.*|\w+=\w?)).*$/);
 
 export {
   isNewLineCharRegExp,
   equalSignRegExp,
   isIgnorablePath,
   regExpCompiler,
+  accetableLinePattern,
 };
