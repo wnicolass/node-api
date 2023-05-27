fca6bbf64d5ff32f300deea59e2b574d9dccb1c8
wnicolass <nicolaswgl99@gmail.com>
2023-05-27 22:44:54 +0100

    feat: create inner callback for readability

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -2,28 +2,23 @@ import { pbkdf2, randomBytes } from 'node:crypto';
 
 export async function hash(password) {
   return await new Promise((res, rej) => {
-    genSalt(16, (err, salt) => {
+    genSalt(16, (err, bufferedSalt) => {
       if (err) {
         return rej(err);
       }
 
       // Node.js does not normalize character representations
       const normalizedPass = password.normalize('NFC');
+      const salt = bufferedSalt.toString('hex');
 
-      pbkdf2(
-        normalizedPass,
-        salt.toString('hex'),
-        100000,
-        64,
-        'sha512',
-        (err, key) => {
-          if (err) {
-            return rej(err);
-          }
-          console.log(key.toString('hex'));
-          res(key.toString('hex'));
+      function hashCb(err, key) {
+        if (err) {
+          return rej(err);
         }
-      );
+        res(key.toString('hex'));
+      }
+
+      pbkdf2(normalizedPass, salt, 100000, 64, 'sha512', hashCb);
     });
   });
 }
