f63e31ba65917c3beaf9e79c4257ab963bb66648
wnicolass <nicolaswgl99@gmail.com>
2023-05-31 15:29:06 +0100

    feat: create sign up route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,22 @@
 import { regExpCompiler } from '../utils/common.js';
-import { encode, decode } from '../utils/unsec-jwt.js';
+import { encode } from '../utils/unsec-jwt.js';
 import { StringDecoder } from 'node:string_decoder';
+import { hash } from '../utils/hash.js';
+import models from '../database/index.js';
 
-const signInRouteRegExp = regExpCompiler(/\/api\/sign-in\/?$/);
-const signUpRouteRegExp = regExpCompiler(/\/api\/sign-up\/?$/);
+const isSignIn = regExpCompiler(/\/api\/sign-in\/?$/)();
+const isSignUp = regExpCompiler(/\/api\/sign-up\/?$/)();
+const isValidEmail = regExpCompiler(
+  /^[^\s\.]+\.?[^\s\.]+@[^\s]+\.[^\s]+\w+$/
+)();
+const isValidPassword = regExpCompiler(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%\]\)]).{6,20}$/
+)();
 
 export default function authRoutes(req, res) {
   let matchedRoute = false;
 
-  if (signInRouteRegExp().test(req.url) && req.method === 'GET') {
+  if (isSignIn.test(req.url) && req.method === 'GET') {
     matchedRoute = true;
     res.writeHead(200, { 'Content-Type': 'application/json' });
     const token = encode({ sub: 'id_here' });
@@ -17,21 +25,55 @@ export default function authRoutes(req, res) {
         access_token: token,
       })
     );
-  } else if (signUpRouteRegExp().test(req.url) && req.method === 'POST') {
+  } else if (isSignUp.test(req.url) && req.method === 'POST') {
     matchedRoute = true;
-    req.on('data', (bufferedBody) => {
+    req.on('data', async (bufferedBody) => {
       const decoder = new StringDecoder('utf8');
-      const { name, email, password } = JSON.parse(decoder.write(bufferedBody));
-      if (!name || !email || !password) {
+      const { username, email, password } = JSON.parse(
+        decoder.write(bufferedBody)
+      );
+
+      if (!username || !email || !password) {
         res.statusCode = 400;
         res.setHeader('Content-Type', 'application/json');
         res.write(
           JSON.stringify({
-            error_msg: 'Required field missing',
+            error_msg: 'Missing required field',
           })
         );
-        res.emit('ready');
+        return res.emit('ready');
       }
+
+      if (!isValidEmail.test(email) || !isValidPassword.test(password)) {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(
+          JSON.stringify({
+            error_msg: 'Invalid credentials',
+          })
+        );
+        return res.emit('ready');
+      }
+
+      const user = await models.User.findOne({ where: { email } });
+      if (user) {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(
+          JSON.stringify({
+            error_msg: 'User already exists',
+          })
+        );
+        return res.emit('ready');
+      }
+
+      return hash(password).then((hashedPassword) => {
+        return models.User.create({
+          username,
+          email,
+          hashed_password: hashedPassword,
+        }).then(() => res.emit('ready'));
+      });
     });
     res.on('ready', () => {
       res.end();
