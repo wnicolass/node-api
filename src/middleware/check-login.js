10875a93817e7c8c5db7fc3d04626af4cf1c1b82
wnicolass <nicolaswgl99@gmail.com>
2023-06-01 21:56:40 +0100

    feat: create http error to properly handle unauth cases

--- src/middleware/check-login.js
+++ src/middleware/check-login.js
@@ -1,37 +1,28 @@
 import { decode } from '../utils/unsec-jwt.js';
 import models from '../database/index.js';
+import HTTPError from '../errors/http.error.js';
 
 export default async function checkLogin(req, res) {
-  let isAuthorized = false;
   const { authorization } = req.headers;
 
   if (!authorization) {
-    res.statusCode = 401;
-    res.setHeader('Content-Type', 'application/json');
-    res.write(
-      JSON.stringify({
-        error_msg: 'Login required',
-      })
-    );
-    res.end();
-    return isAuthorized;
+    throw new HTTPError('Login required', 401, {
+      'Content-Type': 'application/json',
+    });
   }
 
   const [, token] = authorization.split(/\s/);
-
-  const { id, email } = decode(token).payload;
-  const user = await models.User.findOne({ where: { id, email } });
-  if (!user) {
-    res.statusCode = 401;
-    res.setHeader('Content-Type', 'application/json');
-    res.write(
-      JSON.stringify({
-        error_msg: 'Login required',
-      })
-    );
-    res.end();
-    return isAuthorized;
+  let user;
+  try {
+    const { id, email } = decode(token).payload;
+    user = await models.User.findOne({ where: { id, email } });
+  } catch (err) {
+    console.log(err);
   }
-  isAuthorized = true;
-  return isAuthorized;
+
+  if (!user) {
+    throw new HTTPError('Login required', 401, {
+      'Content-Type': 'application/json',
+    });
+  }
 }
--- src/routes/base.routes.js
+++ src/routes/base.routes.js
@@ -3,21 +3,22 @@ import checkLogin from '../middleware/check-login.js';
 
 export default async function baseRoutes(req, res) {
   let matchedRoute = false;
+  try {
+    if (!matchedRoute && isBaseRoute.test(req.url) && req.method === 'GET') {
+      matchedRoute = true;
+      await checkLogin(req, res);
 
-  if (!matchedRoute && isBaseRoute.test(req.url) && req.method === 'GET') {
-    matchedRoute = true;
-    const isAuthorized = await checkLogin(req, res);
-
-    if (!isAuthorized) {
-      return matchedRoute;
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          index: 'ok',
+        })
+      );
     }
-
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(
-      JSON.stringify({
-        index: 'ok',
-      })
-    );
+    return matchedRoute;
+  } catch (err) {
+    res.statusCode = err.statusCode;
+    res.headers = err.headers;
+    res.end(JSON.stringify({ error_msg: err.message }));
   }
-  return matchedRoute;
 }
